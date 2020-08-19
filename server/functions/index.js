8e84a179b784da07214bd23c86470db9e786faad
srlogs <logeshwaran.sr1@gmail.com>
2020-08-20 00:10:07 +0530

    added read api

--- server/functions/index.js
+++ server/functions/index.js
@@ -19,28 +19,47 @@ admin.initializeApp({
 const db = admin.firestore();
 
 app.post('/api/users', (req, res) => {
-    console.log(req.body.name);
+   // console.log(req.body.name);
     (async () => {
-        const userdata = {
-            name : req.body.name,
-            email : req.body.email
-        }
+        
         try {
-            
+            const userdata = {
+                name : req.body.name,
+                email : req.body.email
+            }
             console.log(userdata);
           await db.collection('users').add(userdata);
           return res.status(200).send();
         } catch (error) {
-          console.log(error);
+          //console.log(error);
           return res.status(500).send(error);
         }
       })();
   });
 
-  app.post('/data', (req, res) => {
-      console.log(req.body.name);
-      return res.send();
-  })
+  app.get('/api/read', (req, res) => {
+    (async () => {
+        try {
+            let query = db.collection('users');
+            let response = [];
+            await query.get().then(querySnapshot => {
+            let docs = querySnapshot.docs;
+            for (let doc of docs) {
+                const selectedItem = {
+                    name: doc.data().name,
+                    email: doc.data().email
+                };
+                response.push(selectedItem);
+            }
+            });
+            return res.status(200).send(response);
+        } catch (error) {
+            //console.log(error);
+            return res.status(500).send(error);
+        }
+        })();
+    });
+
 
 
 exports.app = functions.https.onRequest(app);
